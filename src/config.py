74bf24d79079c7e1fdc9fba7618f93d0ef32cdd4
Arne Lieten <arnelieten@mac.home>
2026-06-07 17:39:32 +0200

    feat: fast api server can now receive and handle requests

--- src/config.py
+++ src/config.py
@@ -3,4 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# LLM Providers
 GEMININI_API_KEY = os.environ["GEMINI_API_KEY"]
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+ANTROPIC_API_KEY = os.environ["ANTROPIC_API_KEY"]
+
+# Telegram
+BOTFATHER_API_KEY=os.environ["BOTFATHER_API_KEY"] # auth to set up webhook
+BOTFATHER_WEBHOOK_SECRET=os.environ["BOTFATHER_WEBHOOK_SECRET"] # auth to check requests
+BOTFATHER_LINK=os.environ["BOTFATHER_LINK"]
+BOTFATHER_USER_ID=os.environ["BOTFATHER_USER_ID"]
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from agent_workforce.chat_agent import ChatAgent
+from utils.webhook_auth import authenticate_user_id, authenticate_header_secret
 
 app: FastAPI = FastAPI()
 
@@ -11,9 +12,15 @@ def root():
     return {"Home Page"}
 
 
-@app.post(path="/chat/{message}")
-async def chat(message: str):
-    return await chat_agent.chat(message)
+@app.post(path="/telegram/webhook")
+async def chat(request: Request):
+    request_body = await request.json()
+    request_header = request.headers
+    if authenticate_user_id(request_body=request_body) and authenticate_header_secret(request_header=request_header):
+        message = request_body.get("message")
+        text = message.get("text")
+        response = await chat_agent.chat(text)
+        print(response)
 
 
 # add more endpoints if needed
