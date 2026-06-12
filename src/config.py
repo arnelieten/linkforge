17f63764a6d589e2be05c6b1b2348e3b2add73ee
Arne Lieten <arnelieten@Arnes-MacBook-Air.local>
2026-06-12 08:50:01 +0200

    chore: add documentation for Titus

--- src/config.py
+++ src/config.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# TODO group secrets together in classes
+
 # LLM Providers
 GEMININI_API_KEY = os.environ["GEMINI_API_KEY"]
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
